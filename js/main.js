01c30e367a2ec9082a2b4730eb24e79c12c90f57
Lucas Trapani <ltrapani@etrr.edu.ar>
2022-10-03 20:00:32 -0300

    cuotas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let precioViaje = 0;
-// let cantidadCuotas = 0;
+let cantidadCuotas = 0;
+
 let nombreUsuario = prompt("Ingrese su nombre de usuario");
 alert("Buenos dias "+nombreUsuario);
+
 let viajeElegido = parseInt(prompt("Ingrese el numero de destino al que desea viajar:\n1.Paris\n2.New York\n3.Buenos Aires"));
 let cantidadPasajes = parseInt(prompt("Ingrese la cantidad de pasajes que va a abonar: "));
 
 const precioTotal = (precioViaje, cantidadPasajes) => {return precioViaje*cantidadPasajes}
-// const precioPorCuota = (precioTotal, cantidadCuotas) => {return precioTotal/cantidadCuotas}
+const precioPorCuota = (precioTotal, cantidadCuotas) => {return precioTotal/cantidadCuotas}
 
 
 function ingresarPasajes(){
@@ -42,29 +44,30 @@ function calcularPrecio(){
         break;
         default:
             alert("Error, verifique ingresar correctamente el numero de destino al que desea viajar");
+            
         break;
     }
 }
 
-// function metodoPago(){
-//     let metodoPago = parseInt(prompt("Ingrese el metodo de pago que desea:\n1.Efectivo\n2.Transferencia bancaria"));
-//     switch(metodoPago){
-//         case 1:
-//             alert("Usted ha elegido pagar en efectivo, por lo que debera abonar la totalidad en un solo pago");
-//         break;
-
-//         case 2:
-//             alert("Usted ha elegido pagar con trasnferencia bancaria");
-//             cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea:\n1 cuota\n3 cuotas sin interes\n6 cuotas sin interes"));
-//             console.log("cantidad de cuotas es de "+cantidadCuotas)
-//             alert("El precio por cuota es de "+precioPorCuota(precioTotal, cantidadCuotas)); //REVISAR POR QUE NO ME HACE LA CUENTA
-//         break;
-
-//         default:
-//             alert("Error, verifique ingresar correctamente metodo de pago");
-//         break;
-//     }
-// }
+function metodoPago(){
+    let metodoPago = parseInt(prompt("Ingrese el metodo de pago que desea:\n1.Efectivo\n2.Transferencia bancaria"));
+    switch(metodoPago){
+        case 1:
+            alert("Usted ha elegido pagar en efectivo, por lo que debera abonar la totalidad en un solo pago");
+        break        
+        
+        case 2:
+            alert("Usted ha elegido pagar con trasnferencia bancaria");
+            cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea:\n1 cuota\n3 cuotas sin interes\n6 cuotas sin interes"));
+            console.log("cantidad de cuotas es de "+cantidadCuotas)
+            alert("El precio por cuota es de "+precioPorCuota(precioTotal(precioViaje, cantidadPasajes), cantidadCuotas)+" pesos argentinos"); 
+        break;    
+        
+        default:
+            alert("Error, verifique ingresar correctamente metodo de pago");
+        break;
+    }
+}
 
 calcularPrecio();
-// metodoPago();
+metodoPago();
