e50fd52df0b3305197b2cf381b309f171616d5b0
Lucas Trapani <ltrapani@etrr.edu.ar>
2022-10-12 19:34:07 -0300

    funcion login

--- js/main.js
+++ js/main.js
@@ -1,55 +1,64 @@
-let precioViaje = 0;
-let cantidadCuotas = 0;
+//variables globales
+let precioProducto=0;
+let cantidadCuotas=0;
+let login = false;
 
-let nombreUsuario = prompt("Ingrese su nombre de usuario");
-alert("Buenos dias "+nombreUsuario);
+//funciones flecha
+const precioTotal = (precioProducto, cantidadProductos) => {return precioProducto*cantidadProductos} //calgula precio total del carrito
+const precioPorCuota = (precioTotal, cantidadCuotas) => {return precioTotal/cantidadCuotas} //calcula precio si se elige pagar en cuotas
 
-let viajeElegido = parseInt(prompt("Ingrese el numero de destino al que desea viajar:\n1.Paris\n2.New York\n3.Buenos Aires"));
-let cantidadPasajes = parseInt(prompt("Ingrese la cantidad de pasajes que va a abonar: "));
+//saluda al usuario
+function bienvenido(usuario) {
+    return alert(`Buenos dias ${usuario}`);
+}
 
-const precioTotal = (precioViaje, cantidadPasajes) => {return precioViaje*cantidadPasajes}
-const precioPorCuota = (precioTotal, cantidadCuotas) => {return precioTotal/cantidadCuotas}
+//cargar pedido
+function realizarPedido() {
+    let nombreUsuario = prompt("Ingrese su nombre de usuario");
+    bienvenido(nombreUsuario);
+    let email = prompt("Ingrese su email: ")
 
+    let productoElegido = prompt("Ingresa el producto que desea: \nTeclado \nMonitor \nProcesador");
+    if (productoElegido.toLowerCase() === "teclado" || productoElegido.toLowerCase() === "monitor" || productoElegido.toLowerCase() === "procesador") {
+        console.log(productoElegido);
+    } else {
+        while (productoElegido.toLowerCase() != "teclado" && productoElegido.toLowerCase() != "monitor" && productoElegido.toLowerCase() != "procesador") {
+            productoElegido = prompt("Elegir un producto disponible. Los productos en stock son: \nTeclado \nMonitor \nProcesador");
+        }
+        console.log(productoElegido);
+        }
 
-function ingresarPasajes(){
-    if(0<cantidadPasajes){
-        for(let veces=0; veces<cantidadPasajes; veces++){
-            let nombrePasajero = prompt("Ingrese nombre y apellido del pasajero");
-            alert("Nombre de pasajero numero "+veces+" |"+nombrePasajero+"| "+"registrado correctamente");
+    let cantidadProductos = parseInt(prompt("Ingresa la cantidad de productos que quiere abonar: "));
+    if (parseInt(cantidadProductos === 1)) {
+        alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
+    } else if (parseInt(cantidadProductos)) {
+        alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
+    } else {
+        while (!parseInt(cantidadProductos)) {
+        cantidadProductos = parseInt(prompt("Debes ingresar un numero valido. Vuelve a ingresar el numero de productos que desea abonar"));
+        }
+        if (parseInt(cantidadProductos) === 1) {
+            alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
+        }else {
+            alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
         }
     }
-}
 
-function calcularPrecio(){
-    switch(viajeElegido){
-        case 1:
-            alert("Su viaje a Paris se ha añadido al carrito");
-            precioViaje = 270000;
-            alert("El precio total a abonar es de "+precioTotal(precioViaje, cantidadPasajes)+" pesos argentinos");
-            ingresarPasajes();
-        break;
+    if(productoElegido.toLowerCase() === "teclado"){
+        alert("Su telcado se ha añadido al carrito");
+        precioProducto = 12000;
+        alert("El precio total a abonar es de "+precioTotal(precioProducto, cantidadProductos)+" pesos argentinos");
+    }if(productoElegido.toLowerCase() === "monitor"){
+        alert("Su monitor se ha añadido al carrito");
+        precioProducto = 67000;
+        alert("El precio total a abonar es de "+precioTotal(precioProducto, cantidadProductos)+" pesos argentinos");
+    }if(productoElegido.toLowerCase() === "procesador"){
+        alert("Su procesador ha añadido al carrito");
+        precioProducto = 56000;
+        alert("El precio total a abonar es de "+precioTotal(precioProducto, cantidadProductos)+" pesos argentinos");
+    }else{console.log(productoElegido)}
 
-        case 2:
-            alert("Su viaje a New York se ha añadido al carrito");
-            precioViaje = 310000;
-            alert("El precio total a abonar es de "+precioTotal(precioViaje, cantidadPasajes)+" pesos argentinos");
-            ingresarPasajes();
-        break;
 
-        case 3:
-            alert("Su viaje a Buenos Aires se ha añadido al carrito");
-            precioViaje = 75000;
-            alert("El precio total a abonar es de "+precioTotal(precioViaje, cantidadPasajes)+" pesos argentinos");
-            ingresarPasajes();
-        break;
-        default:
-            alert("Error, verifique ingresar correctamente el numero de destino al que desea viajar");
-            
-        break;
-    }
-}
-
-function metodoPago(){
     let metodoPago = parseInt(prompt("Ingrese el metodo de pago que desea:\n1.Efectivo\n2.Transferencia bancaria"));
     switch(metodoPago){
         case 1:
@@ -57,17 +66,83 @@ function metodoPago(){
         break        
         
         case 2:
-            alert("Usted ha elegido pagar con trasnferencia bancaria");
+            alert("Usted ha elegido pagar con transferencia bancaria");
             cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea:\n1 cuota\n3 cuotas sin interes\n6 cuotas sin interes"));
             console.log("cantidad de cuotas es de "+cantidadCuotas)
-            alert("El precio por cuota es de "+precioPorCuota(precioTotal(precioViaje, cantidadPasajes), cantidadCuotas)+" pesos argentinos"); 
+            alert("El precio por cuota es de "+precioPorCuota(precioTotal(precioProducto, cantidadProductos), cantidadCuotas)+" pesos argentinos"); 
         break;    
         
         default:
             alert("Error, verifique ingresar correctamente metodo de pago");
+            cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea:\n1 cuota\n3 cuotas sin interes\n6 cuotas sin interes"));
         break;
+    }
+    alert(`Tus productos estan reservados, mas tarde nos contactaremos con usted para el envio enviando mas informacion a ${email}`);
+}
+
+//guarda el mail y password para logearse
+function capturarUsuario(){
+    
+    function Persona(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+    var emailCapturar = document.getElementById("email").value;
+    // console.log(emailCapturar);
+    var passwordCapturar = document.getElementById("password").value;
+    // console.log(passwordCapturar);
+
+    nuevoUsuario = new Persona(emailCapturar, passwordCapturar);
+    console.log(nuevoUsuario);
+    agregarUsuario();
+    if (login == true){
+        logearUsuario();
     }
 }
 
-calcularPrecio();
-metodoPago();
+//pushea al usuario en la base de datos
+var baseDatos = [];
+function agregarUsuario(){
+    baseDatos.push(nuevoUsuario);
+    console.log(baseDatos);
+    login = true;
+}
+
+//logea al usuario
+function logearUsuario(){
+    let logear = document.getElementById("login");
+    logear.innerText = `${nuevoUsuario.email}`
+    let boxlogin = document.getElementById("boxlogin")
+    boxlogin.innerHTML = `<a href="#" class="btn btn-danger btn-login" id="cancelar" onclick="deslogearUsuario();" style="display:inline-block !important;">Log out</a>`;
+}
+
+//desloguea al usuario y da lugar a otro logeo
+function deslogearUsuario(){
+    let deslog = document.getElementById("boxlogin");
+    deslog.innerHTML = `<p class="text-center">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="email"><i class="fa fa-envelope" aria-hidden="true"></i> E-mail</label>
+                                    <input type="text" class="form-control" name="email" id="email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password"><i class="fa fa-lock" aria-hidden="true"></i> Contraseña</label>
+                                    <input type="text" class="form-control" name="password" id="password">
+                                </div>
+                                <a href="#!">No recuerdo mi contraseña</a>
+                                <br>
+                                <p class="text-center">
+                                    <a href="#" class="btn btn-danger btn-login" id="cancelar" style="display:inline-block !important;">CANCELAR</a>
+                                    <button type="reset" class="btn btn-primary" onclick="capturarUsuario();">INICIAR SESIÓN</button>
+                                </p>
+                            </form>
+                            <hr>
+                            <p class="text-center">¿Aún no tienes cuenta?</p>
+                            <a href="#!">CRÉATE UNA GRATIS</a>
+                        </p>`;
+    let logear = document.getElementById("login");
+    logear.innerText = "Log in";
+}
+
+realizarPedido();
