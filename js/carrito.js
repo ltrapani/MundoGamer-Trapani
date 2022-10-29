5016e2eeaf1c3dfab626596df415cf86cf64c501
Lucas Trapani <ltrapani@etrr.edu.ar>
2022-10-29 04:32:44 -0300

    carrito funcionando sin css

---
+++ js/carrito.js
@@ -0,0 +1,69 @@
+const imprimirCarrito = () => {
+    pushbarContainer.innerHTML = ""
+    const pushbarOn = document.createElement("div");
+    pushbarOn.className = "pushbar-carrito-header"
+    pushbarOn.setAttribute("id", "pushbarTittle");
+    pushbarOn.innerHTML = `
+    <h2 class="carrito-titulo">Carrito de compras</h2>
+    `;
+    pushbarContainer.append(pushbarOn);
+
+    carrito.forEach((producto) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "pushbar-carrito-content";
+        carritoContent.setAttribute("id", "pushbarCarritoContent")
+        carritoContent.innerHTML = `
+        <h3>${producto.nombre}</h3>
+        <img src="${producto.img}">
+        <p>Precio: ${producto.precio} $</p>
+        <h4>Ingrese la cantidad que desea comprar</h4> 
+        <span class="restar"> - </span>
+        <p>Cantidad: ${producto.cantidad}</p>
+        <span class="sumar"> + </span>
+        <p> Total: ${producto.cantidad * producto.precio} $</p>   
+        `;
+        pushbarContainer.append(carritoContent)
+        console.log(carrito.length)
+        let restar = carritoContent.querySelector(".restar");
+        restar.addEventListener("click", () => {
+            if (producto.cantidad != 1) {
+                producto.cantidad--;
+            }
+            saveLocal();
+            imprimirCarrito();
+        })
+
+        let sumar = carritoContent.querySelector(".sumar");
+        sumar.addEventListener("click", () => {
+            producto.cantidad++;
+            saveLocal();
+            imprimirCarrito();
+        })
+
+        let eliminar = document.createElement("button")
+        eliminar.className = "bi bi-bag-dash icon-carrito"
+        carritoContent.append(eliminar);
+        eliminar.addEventListener("click", () => {
+            eliminarProducto(producto.id);
+        });
+    })
+
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+    const totalComprar = document.createElement("div");
+    totalComprar.className = "pushbar-total-content";
+    totalComprar.setAttribute("id", "pushbarPrecioTotal");
+    totalComprar.innerHTML = `Total a pagar: ${total} $`;
+    pushbarContainer.append(totalComprar);
+};
+
+verCarrito.addEventListener("click", imprimirCarrito);
+
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId;
+    })
+    carritoCounter();
+    saveLocal();
+    imprimirCarrito();
+}
--- js/main.js
+++ js/main.js
@@ -1,89 +1,10 @@
 //variables globales
-let precioProducto=0;
-let cantidadCuotas=0;
 let login = false;
 
-//funciones flecha
-const precioTotal = (precioProducto, cantidadProductos) => {return precioProducto*cantidadProductos} //calgula precio total del carrito
-const precioPorCuota = (precioTotal, cantidadCuotas) => {return precioTotal/cantidadCuotas} //calcula precio si se elige pagar en cuotas
-
-//saluda al usuario
-function bienvenido(usuario) {
-    return alert(`Buenos dias ${usuario}`);
-}
-
-//cargar pedido
-function realizarPedido() {
-    let nombreUsuario = prompt("Ingrese su nombre de usuario");
-    bienvenido(nombreUsuario);
-    let email = prompt("Ingrese su email: ")
-
-    let productoElegido = prompt("Ingresa el producto que desea: \nTeclado \nMonitor \nProcesador");
-    if (productoElegido.toLowerCase() === "teclado" || productoElegido.toLowerCase() === "monitor" || productoElegido.toLowerCase() === "procesador") {
-        console.log(productoElegido);
-    } else {
-        while (productoElegido.toLowerCase() != "teclado" && productoElegido.toLowerCase() != "monitor" && productoElegido.toLowerCase() != "procesador") {
-            productoElegido = prompt("Elegir un producto disponible. Los productos en stock son: \nTeclado \nMonitor \nProcesador");
-        }
-        console.log(productoElegido);
-        }
-
-    let cantidadProductos = parseInt(prompt("Ingresa la cantidad de productos que quiere abonar: "));
-    if (parseInt(cantidadProductos === 1)) {
-        alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
-    } else if (parseInt(cantidadProductos)) {
-        alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
-    } else {
-        while (!parseInt(cantidadProductos)) {
-        cantidadProductos = parseInt(prompt("Debes ingresar un numero valido. Vuelve a ingresar el numero de productos que desea abonar"));
-        }
-        if (parseInt(cantidadProductos) === 1) {
-            alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
-        }else {
-            alert(`Tu orden es de ${cantidadProductos} ${productoElegido}.`);
-        }
-    }
-
-    if(productoElegido.toLowerCase() === "teclado"){
-        alert("Su telcado se ha añadido al carrito");
-        precioProducto = 12000;
-        alert("El precio total a abonar es de "+precioTotal(precioProducto, cantidadProductos)+" pesos argentinos");
-    }if(productoElegido.toLowerCase() === "monitor"){
-        alert("Su monitor se ha añadido al carrito");
-        precioProducto = 67000;
-        alert("El precio total a abonar es de "+precioTotal(precioProducto, cantidadProductos)+" pesos argentinos");
-    }if(productoElegido.toLowerCase() === "procesador"){
-        alert("Su procesador ha añadido al carrito");
-        precioProducto = 56000;
-        alert("El precio total a abonar es de "+precioTotal(precioProducto, cantidadProductos)+" pesos argentinos");
-    }else{console.log(productoElegido)}
-
-
-    let metodoPago = parseInt(prompt("Ingrese el metodo de pago que desea:\n1.Efectivo\n2.Transferencia bancaria"));
-    switch(metodoPago){
-        case 1:
-            alert("Usted ha elegido pagar en efectivo, por lo que debera abonar la totalidad en un solo pago");
-        break        
-        
-        case 2:
-            alert("Usted ha elegido pagar con transferencia bancaria");
-            cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea:\n1 cuota\n3 cuotas sin interes\n6 cuotas sin interes"));
-            console.log("cantidad de cuotas es de "+cantidadCuotas)
-            alert("El precio por cuota es de "+precioPorCuota(precioTotal(precioProducto, cantidadProductos), cantidadCuotas)+" pesos argentinos"); 
-        break;    
-        
-        default:
-            alert("Error, verifique ingresar correctamente metodo de pago");
-            cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas que desea:\n1 cuota\n3 cuotas sin interes\n6 cuotas sin interes"));
-        break;
-    }
-    alert(`Tus productos estan reservados, mas tarde nos contactaremos con usted para el envio enviando mas informacion a ${email}`);
-}
-
 //guarda el mail y password para logearse
-function capturarUsuario(){
-    
-    function Persona(email, password){
+function capturarUsuario() {
+
+    function Persona(email, password) {
         this.email = email;
         this.password = password;
     }
@@ -96,21 +17,22 @@ function capturarUsuario(){
     nuevoUsuario = new Persona(emailCapturar, passwordCapturar);
     console.log(nuevoUsuario);
     agregarUsuario();
-    if (login == true){
+    if (login == true) {
         logearUsuario();
     }
 }
 
 //pushea al usuario en la base de datos
 var baseDatos = [];
-function agregarUsuario(){
+
+function agregarUsuario() {
     baseDatos.push(nuevoUsuario);
     console.log(baseDatos);
     login = true;
 }
 
 //logea al usuario
-function logearUsuario(){
+function logearUsuario() {
     let logear = document.getElementById("login");
     logear.innerText = `${nuevoUsuario.email}`
     let boxlogin = document.getElementById("boxlogin")
@@ -118,7 +40,7 @@ function logearUsuario(){
 }
 
 //desloguea al usuario y da lugar a otro logeo
-function deslogearUsuario(){
+function deslogearUsuario() {
     let deslog = document.getElementById("boxlogin");
     deslog.innerHTML = `<p class="text-center">
                             <form action="">
@@ -146,15 +68,16 @@ function deslogearUsuario(){
 }
 
 
-const carritoContenido = document.getElementById("carritoContenido")
+const carritoContenido = document.getElementById("carritoContenido");
 const verCarrito = document.getElementById("verCarrito");
-const pushbarContainer = document.getElementById("pushbar-container")
+const pushbarContainer = document.getElementById("pushbar-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 
-let carrito = []
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((producto)=>{
+productos.forEach((producto) => {
     let content = document.createElement("div");
-    content.className= "card"
+    content.className = "card"
     content.innerHTML = `
     <img class="card-img-top" src="${producto.img}">
         <div class="card-body">
@@ -167,7 +90,7 @@ productos.forEach((producto)=>{
 
     let comprar = document.createElement("button");
     comprar.className = "btn btn-outline-primary";
-    comprar.innerText = "Agregar al carrito" ;
+    comprar.innerText = "Agregar al carrito";
     content.append(comprar);
 
     let descuento = document.createElement("div");
@@ -175,27 +98,44 @@ productos.forEach((producto)=>{
     descuento.innerHTML = `
         <small >${producto.descuento}</small>
     `;
-    content.append(descuento)
+    content.append(descuento);
 
-    comprar.addEventListener("click", () =>{
-        carrito.push({
-            id : producto.id,
-            img: producto.img,
-            nombre: producto.nombre,
-            precio: producto.precio,
-        })
-        console.log(carrito)
+    comprar.addEventListener("click", () => {
+        const repeat = carrito.some((repeatProducto) => repeatProducto.id === producto.id)
+        if (repeat) {
+            carrito.map((prod) => {
+                if (prod.id === producto.id) {
+                    prod.cantidad++;
+                }
+            })
+        } else {
+            carrito.push({
+                id: producto.id,
+                img: producto.img,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                cantidad: producto.cantidad,
+            })
+        }
+        carritoCounter();
+        saveLocal();
     })
 })
 
-verCarrito.addEventListener("click", () =>{
-    const pushbar = document.createElement("div");
-    pushbar.className = "pushbar-carrito-header"
-    pushbar.innerHTML = `
-    <h2 class="carrito-titulo">Carrito de compras</h2>
-    `;
-    console.log("hola")
-    pushbarContainer.append(pushbar)
-})
+//setear item
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
 
-realizarPedido();
+
+const carritoCounter = () => {
+    cantidadCarrito.style.display = "block";
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"))
+    if (carrito.length === 0) {
+        cantidadCarrito.style.display = "none";
+    }
+}
+
+carritoCounter();
